cbdfb29f8fefed0fe214d6c8743d79ea8a7058a7
Frederick Johnson <fritzj96@gmail.com>
2019-11-23 23:19:41 -0800

    change: multilined a long import

--- client/src/redux/actions/authActions.js
+++ client/src/redux/actions/authActions.js
@@ -1,1 +1,8 @@
-import { LOGIN_ATTEMPT, LOGIN_FAILURE, LOGIN_SUCCESS, SIGNUP_ATTEMPT, SIGNUP_FAILURE, SIGNUP_SUCCESS } from "../constants/authConstants";
+import {
+    LOGIN_ATTEMPT,
+    LOGIN_FAILURE,
+    LOGIN_SUCCESS,
+    SIGNUP_ATTEMPT,
+    SIGNUP_FAILURE,
+    SIGNUP_SUCCESS
+} from "../constants/authConstants";
