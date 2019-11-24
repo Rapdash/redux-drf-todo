a8a889e504cff2c3ec18c9d0644d17027ff1c0e3
Frederick Johnson <fritzj96@gmail.com>
2019-11-24 13:59:49 -0800

    added: started to add tests then decided against it

--- server/todos/tests.py
+++ server/todos/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework.test import APIClient
 
 # Create your tests here.
+
+# class TodoListViewTests(TestCase):
+    # def test_no_todos(self):
+    #     """
+
+    #     """
