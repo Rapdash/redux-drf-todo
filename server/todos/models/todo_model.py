e7d8a27936fa6e1810110fdf8ac848aa4e31ecc3
Frederick Johnson <fritzj96@gmail.com>
2019-11-24 14:04:41 -0800

    fix: added owner member left of the associated foreignKey

--- server/todos/models/todo_model.py
+++ server/todos/models/todo_model.py
@@ -2,7 +2,7 @@ from django.db.models import Model, CharField, BooleanField, ForeignKey, CASCADE
 from django.contrib.auth.models import User
 
 class Todo(Model):
-    ForeignKey(User, on_delete=CASCADE, related_name='todos')
+    owner = ForeignKey(User, on_delete=CASCADE, related_name='todos')
     title = CharField(max_length=30)
     body = CharField(max_length=500)
     checked_off = BooleanField(default=False)
