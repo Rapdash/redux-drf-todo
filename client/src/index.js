ef074875a0e0895647ddb024047074ff0e98c660
Frederick Johnson <fritzj96@gmail.com>
2019-11-23 23:13:57 -0800

    cleanup: organized imports

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import * as serviceWorker from './serviceWorker';
-import { App } from './core/App';
-
 import "@ionic/react/css/ionic.bundle.css";
+
+import { App } from './core/App';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
