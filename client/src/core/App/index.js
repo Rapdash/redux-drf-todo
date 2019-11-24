7e60fc136748174b7bee02c583ec4f213cbb7d82
Frederick Johnson <fritzj96@gmail.com>
2019-11-23 23:12:17 -0800

    cleanup: organized imports

--- client/src/core/App/index.js
+++ client/src/core/App/index.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { Provider } from "react-redux";
 import { IonApp } from "@ionic/react";
+import { Provider } from "react-redux";
 
 import { Router } from "../Router";
 
