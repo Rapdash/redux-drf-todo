f49620b09e982b9bdef560c4a08002dda9dbd48a
Frederick Johnson <fritzj96@gmail.com>
2019-11-24 14:06:20 -0800

    added: get route for TodoListView

--- server/todos/views/todo_views.py
+++ server/todos/views/todo_views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK
+from todos.models.todo_model import Todo
 
 # Create your views here.
+class TodoListView(APIView):
+    def get(self, request, format=None):
+        todos = Todo.objects.filter(owner=request.user.pk)
+        serializer = TodoSerializer(todos, many=True)
+        return Response(serializer.data, HTTP_200_OK)
