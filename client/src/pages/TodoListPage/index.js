8e61c583603276a9e927ed295c7a3eed825e8233
Frederick Johnson <fritzj96@gmail.com>
2019-11-23 22:03:40 -0800

    added: TodoListPage skeleton

---
+++ client/src/pages/TodoListPage/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { IonPage } from "@ionic/react";
+
+const TodoListPageBase = () => {
+    return (
+        <IonPage>
+
+        </IonPage>
+    )   
+}
+
+// TODO: Wire Up Redux
+export const TodoListPage = TodoListPageBase;
