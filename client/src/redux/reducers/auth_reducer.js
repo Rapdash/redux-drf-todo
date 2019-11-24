ed0a3a1172ebd7405d1d33ea954c8eb85af52011
Frederick Johnson <fritzj96@gmail.com>
2019-11-23 22:51:57 -0800

    added: skeleton auth reducer with initialState

---
+++ client/src/redux/reducers/auth_reducer.js
@@ -0,0 +1,26 @@
+const initialState = {
+    /* 
+        Not Sure if Signed In = null
+        Not Signed In = false
+        Signed In = ture
+    */
+    signedIn: null,
+    /* 
+        not (sure if) signed in = null
+        signed in = django user object
+    */
+    user: null,
+    /*
+        not (sure if) signed in = null
+        signed in = jwt string
+    */
+   token: null
+}
+
+export const authReducer = (state = initialState, action) => {
+    switch (action.type) {
+        
+        default:
+            return state;
+    }
+}
