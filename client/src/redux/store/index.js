b0a74b8c6608185679937cb569adfe3cc4f0ee0e
Frederick Johnson <fritzj96@gmail.com>
2019-11-23 23:11:36 -0800

    added: rootReducer import + org'd

--- client/src/redux/store/index.js
+++ client/src/redux/store/index.js
@@ -1,4 +1,6 @@
-import { createStore, applyMiddleware } from "redux";
 import thunk from 'redux-thunk';
+import { createStore, applyMiddleware } from "redux";
+
+import { rootReducer } from "../reducers";
 
 export const store = createStore(rootReducer, applyMiddleware(thunk))
