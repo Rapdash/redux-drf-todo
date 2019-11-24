8dd05a9ce2d739e5e9909262fd10e2bb5a12a331
Frederick Johnson <fritzj96@gmail.com>
2019-11-23 22:57:09 -0800

    added: skeleton todoReducer

---
+++ client/src/redux/reducers/todoReducer.js
@@ -0,0 +1,10 @@
+const initialState = {
+    list: [],
+}
+
+export const todoReducer = (state = initialState, action) => {
+    switch(action.type) {
+        default:
+            return state
+    }
+}
