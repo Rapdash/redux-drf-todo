00a0cd268fb80329184eaa72bd2bca0727b744b4
Frederick Johnson <fritzj96@gmail.com>
2019-11-23 22:03:28 -0800

    added: LoginPage skeleton

---
+++ client/src/pages/LoginPage/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { IonPage } from "@ionic/react";
+
+const LoginPageBase = () => {
+    return (
+        <IonPage>
+
+        </IonPage>
+    )   
+}
+
+// TODO: Wire Up Redux
+export const LoginPage = LoginPageBase;
