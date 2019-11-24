bc4c5258c21a2c03de1f2b9b1a59d571c7436ac1
Frederick Johnson <fritzj96@gmail.com>
2019-11-23 23:13:21 -0800

    cleanup: organized imports

--- client/src/core/Router/index.js
+++ client/src/core/Router/index.js
@@ -1,11 +1,12 @@
 import React from "react";
-import { IonReactRouter } from "@ionic/react-router";
-import { IonRouterOutlet } from "@ionic/react"
 import { Route } from "react-router-dom"
+import { IonRouterOutlet } from "@ionic/react"
+import { IonReactRouter } from "@ionic/react-router";
+
 
 import { ProtectedRoute } from "./ProtectedRoute";
-import { TODOLIST_ROUTE, LOGIN_ROUTE, SIGNUP_ROUTE } from "../../constants/routes";
 import { SignupPage, LoginPage, TodoListPage } from "../../pages";
+import { TODOLIST_ROUTE, LOGIN_ROUTE, SIGNUP_ROUTE } from "../../constants/routes";
 
 export const Router = () => (
     <IonReactRouter>
