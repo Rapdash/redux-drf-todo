f93c8b9843730ab1bc865da175000796166e40ae
Frederick Johnson <fritzj96@gmail.com>
2019-11-23 22:35:18 -0800

    added: LogoutRedirect component that logs out and redirects

---
+++ client/src/shared/LogoutRedirect/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Redirect } from "react-router-dom";
+
+import { LOGIN_ROUTE } from "../../constants/routes";
+
+const LogoutRedirectBase = () => {
+    // TODO: Should log the user out once redux is set up
+    const logout = () => {};
+    return <Redirect to={LOGIN_ROUTE} />
+}
+
+// TODO: Wire Up Redux
+export const LogoutRedirect = LogoutRedirectBase
