594419d9a9f4caea38a6ed5e9a923f7fa3ad776b
Frederick Johnson <fritzj96@gmail.com>
2019-11-23 22:02:15 -0800

    added: SignupPage skeleton

---
+++ client/src/pages/SignupPage/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { IonPage } from "@ionic/react";
+
+const SignupPageBase = () => {
+    return (
+        <IonPage>
+
+        </IonPage>
+    )   
+}
+
+// TODO: Wire Up Redux
+export const SignupPage = SignupPageBase;
