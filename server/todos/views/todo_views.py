f07bdf4c5feaf8144882937c977db2f60f1e84b2
Frederick Johnson <fritzj96@gmail.com>
2019-11-24 14:10:24 -0800

    added: imported todo serializer

--- server/todos/views/todo_views.py
+++ server/todos/views/todo_views.py
@@ -1,9 +1,10 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK
-from todos.models.todo_model import Todo
 
-# Create your views here.
+from todos.models.todo_model import Todo
+from todos.serializers import TodoSerializer
+
 class TodoListView(APIView):
     def get(self, request, format=None):
         todos = Todo.objects.filter(owner=request.user.pk)
