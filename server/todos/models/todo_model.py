34a07198d19dd983fb020388aff237cd827f0235
Frederick Johnson <fritzj96@gmail.com>
2019-11-24 14:22:06 -0800

    added: blank arg to owner field so the serializer can be valid before user is attached.

--- server/todos/models/todo_model.py
+++ server/todos/models/todo_model.py
@@ -2,7 +2,7 @@ from django.db.models import Model, CharField, BooleanField, ForeignKey, CASCADE
 from django.contrib.auth.models import User
 
 class Todo(Model):
-    owner = ForeignKey(User, related_name='todos', on_delete=CASCADE)
+    owner = ForeignKey(User, related_name='todos', on_delete=CASCADE, blank=True)
     title = CharField(max_length=30)
     body = CharField(max_length=500)
     checked_off = BooleanField(default=False)
