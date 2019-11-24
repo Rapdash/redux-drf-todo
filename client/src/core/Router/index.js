4a5ab920db8edc929f5c2ee902724a42f1abb41e
Frederick Johnson <fritzj96@gmail.com>
2019-11-23 22:29:00 -0800

    added: routes & a ProtectedRoute to the router

--- client/src/core/Router/index.js
+++ client/src/core/Router/index.js
@@ -3,10 +3,17 @@ import { IonReactRouter } from "@ionic/react-router";
 import { IonRouterOutlet } from "@ionic/react"
 import { Route } from "react-router-dom"
 
+import { ProtectedRoute } from "./ProtectedRoute";
+import { TODOLIST_ROUTE, LOGIN_ROUTE, SIGNUP_ROUTE } from "../../constants/routes";
+
+import { SignupPage, LoginPage, TodoListPage } from "../../pages";
+
 export const Router = () => (
     <IonReactRouter>
         <IonRouterOutlet>
-            <Route path="/" component={<div />} />
+            <Route path={LOGIN_ROUTE} component={LoginPage} />
+            <Route path={SIGNUP_ROUTE} component={SignupPage} />
+            <ProtectedRoute path={TODOLIST_ROUTE} component={TodoListPage} />
         </IonRouterOutlet>
     </IonReactRouter>
 )
