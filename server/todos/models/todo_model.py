3c369b4ee617227a537ab939f9acab7a48e27933
Frederick Johnson <fritzj96@gmail.com>
2019-11-24 13:34:29 -0800

    added: Todo Model

--- server/todos/models/todo_model.py
+++ server/todos/models/todo_model.py
@@ -1,3 +1,8 @@
-from django.db import models
+from django.db.models import Model, CharField, BooleanField, ForeignKey, CASCADE
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Todo(Model):
+    ForeignKey(User, on_delete=CASCADE, related_name='todos')
+    title = CharField(max_length=30)
+    body = CharField(max_length=500)
+    checked_off = BooleanField(default=False)
