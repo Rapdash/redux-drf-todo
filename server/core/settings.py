4f75ceb4176cb157dc70c2b0eb84b271ec289c65
Frederick Johnson <fritzj96@gmail.com>
2019-11-25 15:07:01 -0800

    removed db name

--- server/core/settings.py
+++ server/core/settings.py
@@ -96,7 +96,6 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 db_from_env = dj_database_url.config(conn_max_age=600)
