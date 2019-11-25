c270300d122f1e8e917c1cd41ed235b0c2f7a25d
Frederick Johnson <fritzj96@gmail.com>
2019-11-25 10:52:14 -0800

    added: delete & check_off reducer cases

--- client/src/redux/reducers/todoReducer.js
+++ client/src/redux/reducers/todoReducer.js
@@ -2,7 +2,11 @@ import {
   ADD_TODO_SUCCESS,
   ADD_TODO_FAILURE,
   GET_TODOS_SUCCESS,
-  GET_TODOS_FAILURE
+  GET_TODOS_FAILURE,
+  DELETE_TODO_SUCCESS,
+  DELETE_TODO_FAILURE,
+  CHECK_OFF_SUCCESS,
+  CHECK_OFF_FAILURE
 } from '../constants/todoConstants';
 
 const initialState = {
@@ -14,18 +18,39 @@ export const todoReducer = (state = initialState, action) => {
   switch (action.type) {
     case GET_TODOS_SUCCESS:
       return Object.assign({}, { error: null, list: action.payload.list });
+
     case GET_TODOS_FAILURE:
       return Object.assign({}, { error: action.payload.error, list: [] });
+
     case ADD_TODO_SUCCESS:
       return Object.assign(
         {},
         { error: null, list: [...state.list, action.payload.todo] }
       );
+
     case ADD_TODO_FAILURE:
-      return Object.assign(
-        {},
-        { error: action.payload.error, list: state.list }
+      return Object.assign({}, { ...state, error: action.payload.error });
+
+    case DELETE_TODO_SUCCESS:
+      const postDeleteList = state.list.filter(
+        todo => todo.id !== action.payload.id
       );
+      return Object.assign({}, { error: null, list: postDeleteList });
+
+    case DELETE_TODO_FAILURE:
+      return Object.assign({}, { ...state, error: action.payload.error });
+
+    case CHECK_OFF_SUCCESS:
+      const postCheckOffList = state.list.map(todo => {
+        if (todo.id === action.payload.id) {
+          return { ...todo, checked_off: !todo.checked_off };
+        }
+        return todo;
+      });
+      return Object.assign({}, { error: null, list: postCheckOffList });
+
+    case CHECK_OFF_FAILURE:
+      return Object.assign({}, { ...state, error: action.payload.error });
     default:
       return state;
   }
