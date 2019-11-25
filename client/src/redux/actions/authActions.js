3eadcde8f281086326749d5de0dea3b6fcedc9dd
Frederick Johnson <fritzj96@gmail.com>
2019-11-25 09:50:16 -0800

    removed: storing token in localstorage. Will add back later

--- client/src/redux/actions/authActions.js
+++ client/src/redux/actions/authActions.js
@@ -14,7 +14,6 @@ export const attemptLogin = (username, password) => (dispatch, getState) => {
   Axios.post(`${process.env.API_ROOT_ENDPOINT}/token`, { username, password })
     .then(res => {
       console.log(res.data);
-      window.localStorage.setItem('auth_token', res.data.access);
       dispatch({
         type: LOGIN_SUCCESS,
         payload: { token: res.data.access }
