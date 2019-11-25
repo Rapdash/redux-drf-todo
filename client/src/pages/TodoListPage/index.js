7d1956e5e8d7abe26f60d051e2c117a862dd285c
Frederick Johnson <fritzj96@gmail.com>
2019-11-25 10:27:38 -0800

    added: skeleton for redux wiring

--- client/src/pages/TodoListPage/index.js
+++ client/src/pages/TodoListPage/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import { IonPage } from '@ionic/react';
+import { connect } from 'react-redux';
 
 const TodoListPageBase = () => {
   return <IonPage></IonPage>;
 };
 
-// TODO: Wire Up Redux
-export const TodoListPage = TodoListPageBase;
+const mapState = state => ({});
+
+const mapDispatch = dispatch => ({});
+
+export const TodoListPage = connect(mapState, mapDispatch)(TodoListPageBase);
