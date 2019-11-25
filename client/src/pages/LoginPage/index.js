b5ecd5f5ecc23267f1d36050224b1c92c382b209
Frederick Johnson <fritzj96@gmail.com>
2019-11-25 10:15:39 -0800

    added: index page

--- client/src/pages/LoginPage/index.js
+++ client/src/pages/LoginPage/index.js
@@ -1,9 +1,95 @@
-import React from 'react';
-import { IonPage } from '@ionic/react';
+import React, { useState } from 'react';
+import { connect } from 'react-redux';
+import { Redirect } from 'react-router-dom';
+import {
+  IonPage,
+  IonHeader,
+  IonToolbar,
+  IonButtons,
+  IonMenuButton,
+  IonTitle,
+  IonContent,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardContent,
+  IonList,
+  IonItem,
+  IonInput,
+  IonLabel,
+  IonButton
+} from '@ionic/react';
 
-const LoginPageBase = () => {
-  return <IonPage></IonPage>;
+import { attemptLogin } from '../../redux/actions/authActions';
+import { TODOLIST_ROUTE } from '../../constants/routes';
+
+const LoginPageBase = ({ token, loading, error, doAttemptLogin }) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  if (token) return <Redirect to={TODOLIST_ROUTE} />;
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar color='primary'>
+          <IonButtons>
+            <IonMenuButton />
+          </IonButtons>
+          <IonTitle>Todo List</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <IonCard>
+          <IonCardHeader>
+            <IonCardTitle class='ion-text-center'>Login</IonCardTitle>
+          </IonCardHeader>
+          <IonCardContent>
+            <IonList>
+              <IonItem>
+                <IonLabel position='floating' color='primary'>
+                  Username
+                </IonLabel>
+                <IonInput
+                  type='text'
+                  value={username}
+                  onChange={e => setUsername(e.target.value)}
+                />
+              </IonItem>
+              <IonItem>
+                <IonLabel position='floating' color='primary'>
+                  Password
+                </IonLabel>
+                <IonInput
+                  type='password'
+                  value={password}
+                  onChange={e => setPassword(e.target.value)}
+                />
+              </IonItem>
+              {error && <IonItem color='danger'>{error.message}</IonItem>}
+              <IonButton
+                expand='block'
+                color='primary'
+                onClick={() => doAttemptLogin(username, password)}
+              >
+                Log In
+              </IonButton>
+            </IonList>
+          </IonCardContent>
+        </IonCard>
+      </IonContent>
+    </IonPage>
+  );
 };
 
 // TODO: Wire Up Redux
-export const LoginPage = LoginPageBase;
+const mapState = state => ({
+  loading: state.auth.loading,
+  error: state.auth.error,
+  token: state.auth.token
+});
+const mapDispatch = dispatch => ({
+  doAttemptLogin: (username, password) =>
+    dispatch(attemptLogin(username, password))
+});
+
+export const LoginPage = connect(mapState, mapDispatch)(LoginPageBase);
