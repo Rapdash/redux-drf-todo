2b9890c4f6c734ff688993190dbea6e451185025
Frederick Johnson <fritzj96@gmail.com>
2019-11-24 18:38:46 -0800

    added: token-view for simple jwt

--- server/core/urls.py
+++ server/core/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import TokenObtainPairView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('todos.urls')),
+    path('token/', TokenObtainPairView.as_view(), name="token-view"),
 ]
