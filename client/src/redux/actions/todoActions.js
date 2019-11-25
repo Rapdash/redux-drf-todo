107d4d1b23582ff9c80674128c2fee4842a32062
Frederick Johnson <fritzj96@gmail.com>
2019-11-25 13:32:40 -0800

    added: checkOff & delete Todo actions

--- client/src/redux/actions/todoActions.js
+++ client/src/redux/actions/todoActions.js
@@ -6,17 +6,20 @@ import {
   // DELETE_TODO_FAILURE,
   // DELETE_TODO_SUCCESS,
   GET_TODOS_FAILURE,
-  GET_TODOS_SUCCESS
+  GET_TODOS_SUCCESS,
+  CHECK_OFF_SUCCESS,
+  CHECK_OFF_FAILURE,
+  DELETE_TODO_SUCCESS,
+  DELETE_TODO_FAILURE
 } from '../constants/todoConstants';
 
 export const addTodo = (title, body, token) => (dispatch, getState) => {
   console.log(getState());
-  fetch('http://localhost:8000', {
+  fetch('http://localhost:8000/', {
     method: 'POST',
     body: JSON.stringify({ body, title }),
     headers: {
-      Authorization: 'Bearer ' + token,
-      'Content-Type': 'application/json'
+      Authorization: 'Bearer ' + token
     }
   })
     .then(res => {
@@ -30,7 +33,7 @@ export const addTodo = (title, body, token) => (dispatch, getState) => {
 };
 
 export const getTodos = token => (dispatch, getState) => {
-  fetch('http://localhost:8000', {
+  fetch('http://localhost:8000/', {
     headers: { Authorization: 'Bearer ' + token }
   })
     .then(res => {
@@ -40,5 +43,34 @@ export const getTodos = token => (dispatch, getState) => {
     })
     .catch(error => {
       dispatch({ type: GET_TODOS_FAILURE, payload: { error } });
+    });
+};
+
+export const checkOffTodo = (todo, token) => (dispatch, getState) => {
+  fetch(`http://localhost:8000/${todo.id}/`, {
+    method: 'PUT',
+    body: JSON.stringify({ ...todo, checked_off: !todo.checked_off }),
+    headers: { Authorization: 'Bearer ' + token }
+  })
+    .then(res => {
+      res.json().then(data => {
+        dispatch({ type: CHECK_OFF_SUCCESS, payload: { todo: data } });
+      });
+    })
+    .catch(error => {
+      dispatch({ type: CHECK_OFF_FAILURE, payload: { error } });
+    });
+};
+
+export const deleteTodo = (todo, token) => (dispatch, getState) => {
+  fetch(`http://localhost:8000/${todo.id}/`, {
+    method: 'DELETE',
+    headers: { Authorization: 'Bearer ' + token }
+  })
+    .then(res => {
+      dispatch({ type: DELETE_TODO_SUCCESS, payload: { id: todo.id } });
+    })
+    .catch(error => {
+      dispatch({ type: DELETE_TODO_FAILURE, payload: { error } });
     });
 };
