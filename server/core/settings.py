b4205a6f0b15494ef9a32295eead0c86a386eef4
Frederick Johnson <fritzj96@gmail.com>
2019-11-25 15:13:46 -0800

    added: hopefully the last version of Database config

--- server/core/settings.py
+++ server/core/settings.py
@@ -93,8 +93,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-DATABASES = {
-}
+DATABASES = {}
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
 
