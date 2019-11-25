7dc9b35b6810b175ff52d9c9044d6946adebde20
Frederick Johnson <fritzj96@gmail.com>
2019-11-25 15:15:35 -0800

    added: different structure for the db string

--- server/core/settings.py
+++ server/core/settings.py
@@ -94,8 +94,7 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 DATABASES = {}
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+DATABASES['default'] = dj_database_url.config(conn_max_age=600)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
