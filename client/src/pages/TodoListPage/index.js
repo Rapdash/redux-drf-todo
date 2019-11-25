1846402d64e3bc931d923386029680c680f561a3
Frederick Johnson <fritzj96@gmail.com>
2019-11-25 11:10:21 -0800

    added: mapState & mapDispatch functions

--- client/src/pages/TodoListPage/index.js
+++ client/src/pages/TodoListPage/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import { IonPage } from '@ionic/react';
 import { connect } from 'react-redux';
+import { getTodos, addTodo } from '../../redux/actions/todoActions';
 
 const TodoListPageBase = () => {
   return <IonPage></IonPage>;
 };
 
-const mapState = state => ({});
+const mapState = state => ({
+  list: state.todos.list,
+  error: state.todos.error
+});
 
-const mapDispatch = dispatch => ({});
+const mapDispatch = dispatch => ({
+  doGetTodos: token => dispatch(getTodos()),
+  doAddTodo: (title, body, token) => dispatch(addTodo(title, body, token))
+});
 
 export const TodoListPage = connect(mapState, mapDispatch)(TodoListPageBase);
