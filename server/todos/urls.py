3b57ee4801eeeb3e8ab3f86b6a7e092985443645
Frederick Johnson <fritzj96@gmail.com>
2019-11-24 18:16:49 -0800

    added: path names to todo urlconfig

--- server/todos/urls.py
+++ server/todos/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from todos.views.todo_views import TodoListView, TodoDetailView
 
 urlpatterns = [
-    path('', TodoListView.as_view()),
-    path('<int:pk>/', TodoDetailView.as_view()),
+    path('', TodoListView.as_view(), name="todo-list"),
+    path('<int:pk>/', TodoDetailView.as_view(), name="todo-detail"),
 ]
