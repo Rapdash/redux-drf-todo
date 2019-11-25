9fabb938779c1d918907b2624c13a0d99a375afc
Frederick Johnson <fritzj96@gmail.com>
2019-11-25 13:19:21 -0800

    added: checkoff button & variable color

--- client/src/pages/TodoListPage/TodoCard/index.js
+++ client/src/pages/TodoListPage/TodoCard/index.js
@@ -3,16 +3,20 @@ import {
   IonCard,
   IonCardHeader,
   IonCardTitle,
-  IonCardContent
+  IonCardContent,
+  IonButton
 } from '@ionic/react';
 
 export const TodoCard = ({ todo }) => (
   <IonCard>
-    <IonCardHeader>
+    <IonCardHeader color={todo.checked_off ? 'success' : 'danger'}>
       <IonCardTitle className='ion-text-center'>{todo.title}</IonCardTitle>
     </IonCardHeader>
-    <IonCardContent>
-      <p className='ion-text-center'>{todo.body}</p>
+    <IonCardContent className='ion-text-center'>
+      <p>{todo.body}</p>
+      <IonButton color='primary'>
+        {todo.checked_off ? 'Completed' : 'Not Completed'}
+      </IonButton>
     </IonCardContent>
   </IonCard>
 );
