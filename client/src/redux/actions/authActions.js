96613c5efa551a50eee12ee43d02c54c5775da29
Frederick Johnson <fritzj96@gmail.com>
2019-11-25 09:24:48 -0800

    added: attemptLogin action

--- client/src/redux/actions/authActions.js
+++ client/src/redux/actions/authActions.js
@@ -6,3 +6,21 @@ import {
   SIGNUP_FAILURE,
   SIGNUP_SUCCESS
 } from '../constants/authConstants';
+
+import Axios from 'axios';
+
+export const attemptLogin = (username, password) => (dispatch, getState) => {
+  dispatch({ type: LOGIN_ATTEMPT });
+  Axios.post(`${process.env.BACKEND_HOST}/token`, { username, password })
+    .then(res => {
+      console.log(res.data);
+      dispatch({
+        type: LOGIN_SUCCESS,
+        payload: { token: res.data.access_token }
+      });
+    })
+    .catch(error => {
+      console.log(error);
+      dispatch({ type: LOGIN_FAILURE, payload: { error } });
+    });
+};
