1108828e17c18d6de074bc13f6824ae350826b8d
Frederick Johnson <fritzj96@gmail.com>
2019-11-24 17:55:18 -0800

    added: progress on todoDetail & docstrings for views

--- server/todos/views/todo_views.py
+++ server/todos/views/todo_views.py
@@ -1,11 +1,15 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.status import HTTP_200_OK, HTTP_201_CREATED, HTTP_400_BAD_REQUEST
+from django.http import Http404
 
 from todos.models.todo_model import Todo
 from todos.serializers import TodoSerializer
 
 class TodoListView(APIView):
+    """
+    Create a todo or get all todos associated with your account
+    """
     def get(self, request, format=None):
         todos = Todo.objects.filter(owner=request.user.pk)
         serializer = TodoSerializer(todos, many=True)
@@ -17,3 +21,16 @@ class TodoListView(APIView):
             serializer.save(owner=request.user)
             return Response(serializer.data, HTTP_201_CREATED)
         return Response(serializer.errors, HTTP_400_BAD_REQUEST)
+
+class TodoDetailView(APIView):
+    """
+    Interact with individual Todos
+    """
+    def get_todo(self, pk):
+        try:
+            return Todo.objects.get(pk=pk)
+        except Todo.doesNotExist:
+            raise Http404
+
+    def put(self, request, format=None):
+        todo = Todo.objects.
