cb30312bd4d03ae720fd73e44cff13f49b9f7329
Frederick Johnson <fritzj96@gmail.com>
2019-11-25 11:16:01 -0800

    updated protectedRoute to work with redux

--- client/src/core/Router/ProtectedRoute/index.js
+++ client/src/core/Router/ProtectedRoute/index.js
@@ -1,18 +1,23 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import { LOGIN_ROUTE } from '../../../constants/routes';
 
-const ProtectedRouteBase = ({ component: Component, ...rest }) => {
+const ProtectedRouteBase = ({ token, component: Component, ...rest }) => {
   return (
     <Route
       {...rest}
       render={props =>
         // TODO: Make this respond to redux auth state instead of just being jammed open
-        true ? <Component {...props} /> : <Redirect to={LOGIN_ROUTE} />
+        token ? <Component {...props} /> : <Redirect to={LOGIN_ROUTE} />
       }
     />
   );
 };
 
-export const ProtectedRoute = ProtectedRouteBase;
+const mapState = state => ({
+  token: state.auth.token
+});
+
+export const ProtectedRoute = connect(mapState)(ProtectedRouteBase);
